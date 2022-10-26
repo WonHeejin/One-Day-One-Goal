375d9fa34011cea642d401bbaaace8f5ee095ead
WonHeejin <youth772@naver.com>
2022-10-26 16:33:49 +0900

    상단 리스트 고정, drop 시 카테고리 별 색상 적용, 여러개 동시 이동 가능

--- src/main/resources/static/js/app/goal.js
+++ src/main/resources/static/js/app/goal.js
@@ -1,12 +1,36 @@
 var example2Left = document.getElementById('example1');
 var example2Right = document.getElementById('example2');
-alert(example2Left);
+var example3 = document.getElementById('example3');
+
 new Sortable(example2Left, {
+    ghostClass: 'goal-background-class',
+    multiDrag: true, // Enable multi-drag
+	selectedClass: 'selected', // The class applied to the selected items
     group: 'shared', // set both lists to same group
-    animation: 150
+    animation: 150,
+    onMove(e) { //static 클래스가 아닌 요소들만 움직이도록
+         return e.related.className.indexOf('static') === -1;
+       }
 });
 
 new Sortable(example2Right, {
+    ghostClass: 'success-background-class',
+    multiDrag: true, // Enable multi-drag
+	selectedClass: 'selected', // The class applied to the selected items
     group: 'shared',
-    animation: 150
+    animation: 150,
+    onMove(e) { //static 클래스가 아닌 요소들만 움직이도록
+         return e.related.className.indexOf('static') === -1;
+       }
+});
+
+new Sortable(example3, {
+    ghostClass: 'fail-background-class',
+    multiDrag: true, // Enable multi-drag
+	selectedClass: 'selected', // The class applied to the selected items
+    group: 'shared',
+    animation: 150,
+    onMove(e) { //static 클래스가 아닌 요소들만 움직이도록
+         return e.related.className.indexOf('static') === -1;
+       }
 });
